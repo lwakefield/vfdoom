51aac6b456c839ad082c99a7b1754c79493dcc41
Lawrence Wakefield <lawrence@iamlawrence.me>
2016-11-13 16:05:17 -0500

    Adds basic node tree and patcher (traverser)

---
+++ index.js
@@ -0,0 +1,173 @@
+const noop = () => {}
+const newEl = (tagName) => document.createElement(tagName)
+
+export class Node {
+  children = []
+  parent = null
+  prevSibling = null
+  nextSibling = null
+
+  addChild (child) {
+    this.children.push(child)
+    child.parent = this
+
+    const len = this.children.length
+    if (len < 2) return
+
+    const lastChild = this.children[len - 2]
+    lastChild.nextSibling = child
+    child.prevSibling = lastChild
+  }
+
+  get firstChild () {
+    const children = this.children
+    return children.length ? children[0] : null
+  }
+}
+
+export class Component extends Node {
+  attributes = []
+  mounted = new Map()
+  recycled = {}
+  constructor (componentName, tagName='div') {
+    super()
+    this.componentName = componentName
+    this.tagName = tagName
+  }
+  recycle (node) {
+    const mountKey = node._mountKey
+    if (mountKey) {
+      mounted[mountKey] = node
+    }
+    node.parent.removeChild(node)
+  }
+}
+
+export class Vnode extends Node {
+  attributes = []
+  constructor (tagName) {
+    super()
+    this.tagName = tagName
+  }
+}
+
+export class Vfnode extends Node {
+  constructor (fn = noop) {
+    super()
+    this.fn = fn
+  }
+  get children () {
+    return this.fn()
+  }
+}
+
+export class Patcher {
+  mountPoint = 0
+  constructor (node, component) {
+    this.component = component
+    this.nodeA = node
+    this.nodeB = node
+  }
+  patch () {
+
+  }
+  next () {
+    // down
+    // right
+    // up and across
+    if (!this.nodeB) return undefined
+
+    this.mountPoint++
+
+    if (this.nodeB.firstChild) return this._down()
+    if (this.nodeB.nextSibling) return this._across()
+    return this._upAndAcross()
+  }
+  _down () {
+    this.nodeB = this.nodeB.firstChild
+    if (!this.nodeA.firstChild) {
+      this.nodeA.appendChild(newEl(this.nodeB.tagName))
+    }
+    this.nodeA = this.nodeA.firstChild
+    return this.nodeB
+  }
+  _across () {
+    this.nodeB = this.nodeB.nextSibling
+    if (!this.nodeB.nextSibling) {
+      this.nodeA.parent.appendChild(newEl(this.nodeB.tagName))
+    }
+    this.nodeA = this.nodeA.nextSibling
+    return this.nodeB
+  }
+  _upAndAcross () {
+    if (!this.nodeB.parent.nextSibling) return undefined
+
+    while (!this.nodeB.parent.nextSibling) {
+      this.nodeB = this.nodeB.parent
+      this.nodeA = this.nodeA.parent
+    }
+    this.nodeB = this.nodeB.parent.nextSibling
+    if (!this.nodeA.parent.nextSibling) {
+      this.nodeA.parent.parent.appendChild(newEl(this.nodeB.tagName))
+    }
+    this.nodeA = this.nodeA.parent.nextSibling
+    return this.nodeB
+  }
+}
+
+/**
+ * The top level node must be a component, because we need to manage which
+ * vnodes are mounted at any given time
+ */
+function patch(node, component) {
+  let mountPoint = 0
+
+  function _patch(nodeA, nodeB) {
+    if (!nodeB.mountPoint) nodeB.mountPoint = mountPoint++
+    const mountKey = `${node.mountPoint}.${nodeB.key || 0}`
+
+    const mounted = component.mounted[mountKey]
+    if (mounted && nodeA !== mounted) {
+      component.recycle(nodeA)
+      nodeA = mounted
+    } if (!mounted) {
+      component.mounted[mountKey] = nodeA
+    }
+
+    _patchChildren(nodeA, nodeB)
+  }
+
+  function _patchChildren(nodeA, nodeB) {
+    let childA = nodeA.firstChild
+    let childB = nodeB.firstChild
+    while (childA && childB) {
+      if (childB instanceof Vfnode) {
+        childB = childB.firstChild
+        _giveFunctionalChildrenMountPoint(childB)
+        continue
+      }
+      _patch(childA, childB)
+
+      childB = childB.nextSibling
+      // There is a next round and we dont have a real DOM node ready...
+      if (childB && !childA.nextSibling) {
+        const emptyNode = document.createChild(childB.tagName)
+        childA.parent.appendChild(emptyNode)
+      }
+      childA = childA.nextSibling
+    }
+    cleanUp(childA)
+  }
+
+  function cleanUp(node) {
+    while (node) {
+      component.recycle(node)
+      node = node.nextSibling
+    }
+  }
+
+  function _giveFunctionalChildrenMountPoint(child) {
+    while (child) child.mountPoint = mountPoint
+  }
+}
+
---
+++ index.spec.js
@@ -0,0 +1,121 @@
+/* eslint-env mocha */
+import {expect} from 'chai'
+import {
+  Node,
+  Patcher,
+} from './index.js'
+
+function treeFromStr (str) {
+  const relations = str.split('\n').map(v => v.trim()).filter(v => !!v)
+  const nodeNames = new Set()
+  for (const relation of relations) {
+    const [left, right] = relation.split(' - ')
+    nodeNames.add(left)
+    nodeNames.add(right)
+  }
+  const nodes = new Map()
+  for (const nodeName of nodeNames) {
+    nodes.set(nodeName, new Node())
+  }
+  for (const relation of relations) {
+    const [left, right] = relation.split(' - ')
+    const leftNode = nodes.get(left)
+    const rightNode = nodes.get(right)
+    leftNode.addChild(rightNode)
+  }
+  for (const node of nodes.values()) {
+    if (!node.parent) return node
+  }
+}
+
+describe('Node', () => {
+  it('adds a single child', () => {
+    const nodeA = new Node()
+    const nodeB = new Node()
+    nodeA.addChild(nodeB)
+
+    expect(nodeA.firstChild).to.eql(nodeB)
+    expect(nodeA.children[0]).to.eql(nodeB)
+    expect(nodeB.parent).to.eql(nodeA)
+  })
+
+  it('adds two children', () => {
+    const nodeA = new Node()
+    const nodeB = new Node()
+    const nodeC = new Node()
+    nodeA.addChild(nodeB)
+    nodeA.addChild(nodeC)
+
+    expect(nodeA.firstChild).to.eql(nodeB)
+    expect(nodeA.children[0]).to.eql(nodeB)
+    expect(nodeB.parent).to.eql(nodeA)
+
+    expect(nodeA.firstChild.nextSibling).to.eql(nodeC)
+    expect(nodeA.children[1]).to.eql(nodeC)
+    expect(nodeB.nextSibling).to.eql(nodeC)
+    expect(nodeC.parent).to.eql(nodeA)
+    expect(nodeC.prevSibling).to.eql(nodeB)
+  })
+})
+
+describe('treeFromStr', () => {
+  it('builds a simple tree', () => {
+    const nodeA = treeFromStr(`
+    A - B
+    A - D
+    B - C
+    `)
+
+    expect(nodeA).to.be.ok
+    expect(nodeA).to.be.instanceof(Node)
+
+    const nodeB = nodeA.firstChild
+    expect(nodeB).to.be.ok
+    expect(nodeB).to.be.instanceof(Node)
+    expect(nodeB.parent).to.eql(nodeA)
+
+    const nodeC = nodeB.firstChild
+    expect(nodeC).to.be.ok
+    expect(nodeC).to.be.instanceof(Node)
+    expect(nodeC.parent).to.eql(nodeB)
+
+    const nodeD = nodeA.children[1]
+    expect(nodeD).to.be.ok
+    expect(nodeD).to.be.instanceof(Node)
+    expect(nodeD.parent).to.eql(nodeA)
+    expect(nodeB.nextSibling).to.eql(nodeD)
+    expect(nodeD.prevSibling).to.eql(nodeB)
+  })
+})
+
+describe('Patcher', () => {
+  //     A
+  //    / \
+  //   B   D
+  //  /
+  // C
+  const nodeA = treeFromStr(`
+    A - B
+    A - D
+    B - C
+  `)
+  it('iterates correctly', () => {
+    const patcher = new Patcher(nodeA, nodeA)
+    const nodeB = nodeA.firstChild
+    const nodeC = nodeB.firstChild
+    const nodeD = nodeA.firstChild.nextSibling
+
+    expect(patcher.mountPoint).to.eql(0)
+
+    expect(patcher.next()).to.eql(nodeB)
+    expect(patcher.mountPoint).to.eql(1)
+
+    expect(patcher.next()).to.eql(nodeC)
+    expect(patcher.mountPoint).to.eql(2)
+
+    expect(patcher.next()).to.eql(nodeD)
+    expect(patcher.mountPoint).to.eql(3)
+
+    expect(patcher.next()).to.be.undefined
+  })
+})
